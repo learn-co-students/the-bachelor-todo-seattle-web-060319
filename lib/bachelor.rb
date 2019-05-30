33ba7150f9b4038529550ca542ed2be2e308697d
Justin <sumbhodi1618@gmail.com>
2019-05-30 20:04:48 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,21 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |individual|
+    if individual["status"] == "Winner"
+       return individual["name"].split[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, person|
+    person.each do |statss|
+      if statss["occupation"] == occupation
+          return statss["name"]
+             #binding.pry
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
